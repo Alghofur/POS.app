42ae615987d98778f15b7dc9cfc4e2e22f78cbf4
Alghofur <awalludin.i.a.g@gmail.com>
2022-06-16 11:01:01 +0700

    alhamdulillah

---
+++ Point_of_sale/Controllers/PengaturanController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Point_of_sale.Controllers
+{
+    public class PengaturanController : Controller
+    {
+        // GET: Pengaturan
+        public ActionResult Pengaturan()
+        {
+            return View();
+        }
+    }
+}
